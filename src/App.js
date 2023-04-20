2eff30c5c91d10202e4db13bbce7df5ddeec54b2
Aryann15 <rajlysm15@gmail.com>
2023-04-20 17:50:09 +0530

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, { useState, useRef }  from "react";
 
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
+import data from "./utils.js";
+
+import Library from "./components/Library"
 
 function App() {
+  const audioRef = useRef(null);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying,setIsPlaying]=useState(false)
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+
+
   return (
     <div className="App">
-      
+      <h1> JukeJam</h1>
+      <Song currentSong={currentSong}/>
+      <Player 
+      audioRef={audioRef}
+      isPlaying={isPlaying}
+      setIsPlaying={setIsPlaying}
+      currentSong={currentSong}
+      setSongInfo={setSongInfo}
+      songInfo={songInfo}/>
+
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 }
